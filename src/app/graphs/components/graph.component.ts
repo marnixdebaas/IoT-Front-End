d3321468feb75e61f43ef2394b915d239fde3d8d
vlcbrand <vlcbrand@avans.nl>
2018-04-05 12:21:20 +0200

    styled graph page & added loading spinners

--- src/app/graphs/components/graph.component.ts
+++ src/app/graphs/components/graph.component.ts
@@ -11,7 +11,7 @@ import moment = require('moment');
 @Component({
   selector: 'app-graph-component',
   templateUrl: './graph.component.html',
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./graph.component.css']
 })
 
 export class GraphComponent implements OnInit {
@@ -36,7 +36,10 @@ export class GraphComponent implements OnInit {
     public graphData: SensorData[];
     public graphDataAll: SensorData[];
     public kWhAll: number;
+    public kWhDates:number;
     public isVisible: boolean;
+    public isVisibleAll: boolean;
+    public loading: boolean;
     // The Constructor
     constructor(public apiService: AppService, private cdr: ChangeDetectorRef) {
         this.single = single;
@@ -57,11 +60,12 @@ export class GraphComponent implements OnInit {
         this.showYAxisLabel = true;
         this.yAxisLabel = 'kWh';
         this.colorScheme = {
-            domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+            domain: ['#E91E63', '#A10A28', '#C7B42C', '#AAAAAA']
         };
-        this.view = [700, 500];
+        this.view = [650 , 450];
         this.graphData = [];
         this.graphDataAll = [];
+        this.isVisibleAll = false;
 
 
         const firstDate = new Date('2018-03-29 11:57:52.374').toDateString();
@@ -76,10 +80,12 @@ export class GraphComponent implements OnInit {
           }
           this.cdr.detectChanges();
           this.kWhAll = this.graphDataAll[0].series[4].value;
+          this.isVisibleAll = true;
         });
     }
 
     sendRequest($event) {
+        this.loading = true;
         var fromDate: string = this.fromDate.toString();
         var toDate: string = this.toDate.toString();
         var dateDifference: number = Date.parse(toDate) - Date.parse(fromDate);
@@ -96,6 +102,8 @@ export class GraphComponent implements OnInit {
                     this.graphData[0].series[d].name = moment(this.graphData[0].series[d].name).format('MM/DD/YYYY H:mm');
                 }
                 this.cdr.detectChanges();
+                this.kWhDates = this.graphDataAll[0].series[4].value;
+                this.loading = false;
                 this.isVisible = true;
             });
         }
